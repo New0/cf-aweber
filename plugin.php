7f10fb0e1ff1e76479cd70932e19ca7f16797e3f
Josh Pollock <Josh@JoshPress.net>
2016-08-24 14:24:07 -0400

    Version 1.0.3

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Aweber for Caldera Forms
  * Plugin URI:  https://calderawp.com
  * Description: Aweber newsletter integration for Caldera Forms
- * Version: 1.0.3-b-1
+ * Version: 1.0.3
  * Author:      Josh Pollock for CalderaWP LLC
  * Author URI:  https://CalderaWP.com
  * License:     GPLv2+
@@ -33,7 +33,7 @@
 /**
  * Define constants
  */
-define( 'CF_AWEBER_VER', '1.0.3-b-1' );
+define( 'CF_AWEBER_VER', '1.0.3' );
 define( 'CF_AWEBER_URL',     plugin_dir_url( __FILE__ ) );
 define( 'CF_AWEBER_PATH',    dirname( __FILE__ ) . '/' );
 define( 'CF_AWEBER_CORE',    dirname( __FILE__ )  );
